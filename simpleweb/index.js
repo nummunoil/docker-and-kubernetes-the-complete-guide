5ce2468cece3557c5f2882319c73a3a5f6fa74ac
Oil Sudarat <sudarat@enres.co>
2020-08-27 12:35:14 +0700

    Copying Build Files

--- simpleweb/index.js
+++ simpleweb/index.js
@@ -6,7 +6,6 @@ app.get("/", (req, res) => {
   res.send("Hi there");
 });
 
-app,
-  listen(8080, () => {
-    console.log("Listening on port 8080");
-  });
+app.listen(8080, () => {
+  console.log("Listening on port 8080");
+});
